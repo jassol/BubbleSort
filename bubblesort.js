cd6f796d19c5fe7ba6b8a1d2b856fcd99ff3b8b9
Jasmin Soltani <jksoltani@gmail.com>
2020-02-18 15:22:40 -0500

    Completed with Sooin

--- bubblesort.js
+++ bubblesort.js
@@ -1,15 +1,36 @@
-function bubbleSort(array) {
-  return []
-}
+// function bubbleSort(array) {
+//   return []
+// }
 
+// function swapHelper(array) {
+//   for(i=0; i<array.length; i++) {
+//     if (array[i] > array [i+1]) {
+//       let temp = array[i]
+//       array[i] = array[i+1]
+//       array[i+1] = temp
+//     }
+//   }
+
+//   return array
+// }
+
+
+function bubbleSort(array) {
+  let len = array.length;
+  let result = array;
+  for (i = 0; i < len; i++) {
+    result = swapHelper(result);
+    console.log(swapHelper(result));
+  }
+  return result;
+}
 function swapHelper(array) {
-  for(i=0; i<array.length; i++) {
-    if (array[i] > array [i+1]) {
-      let temp = array[i]
-      array[i] = array[i+1]
-      array[i+1] = temp
+  for (i = 0; i < array.length; i++) {
+    if (array[i] > array[i + 1]) {
+      let temp = array[i];
+      array[i] = array[i + 1];
+      array[i + 1] = temp;
     }
   }
-
-  return array
+  return array;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,21 +1,44 @@
-describe('Bubble Sort Function', function(){
+// describe('Bubble Sort Function', function(){
 
 
-  it('handles an empty array', function(){
-    expect( bubbleSort([]) ).toEqual( [] );
+//   it('handles an empty array', function(){
+//     expect( bubbleSort([]) ).toEqual( [] );
+//   });
+
+// });
+
+
+// describe('Swap Helper Function', function(){
+
+//   it('handles an array with two elements', function(){
+//     expect( swapHelper( [1,2]) ).toEqual( [1,2] );
+//   });
+
+//   it('swaps elements if the first element is less than the one adjacent', function(){
+//     expect( swapHelper( [2,1]) ).toEqual( [1,2] );
+//   });
+
+// });
+
+describe(‘Bubble Sort Function’, function() {
+  it(‘handles an empty array’, function() {
+    expect(bubbleSort([])).toEqual([]);
   });
-
+  // it(‘handles an array that require more than one swap’, function() {
+  //   expect(bubbleSort([1, 2, 1, 0, 3, 5])).toEqual([0, 1, 1, 2, 3, 5]);
+  // });
+  it(‘handles an array that require more than one swap’, function() {
+    expect(bubbleSort([1, 2, 1, 0])).toEqual([0, 1, 1, 2]);
+  });
 });
-
-
-describe('Swap Helper Function', function(){
-
-  it('handles an array with two elements', function(){
-    expect( swapHelper( [1,2]) ).toEqual( [1,2] );
+describe(‘Swap Helper Function’, function() {
+  it(‘handles an array with two elements’, function() {
+    expect(swapHelper([1, 2])).toEqual([1, 2]);
   });
-
-  it('swaps elements if the first element is less than the one adjacent', function(){
-    expect( swapHelper( [2,1]) ).toEqual( [1,2] );
+  it(‘handles an array with more than two elements’, function() {
+    expect(swapHelper([1, 2, 1, 3, 5])).toEqual([1, 1, 2, 3, 5]);
   });
-
+  // it(‘swaps elements if the first element is less than the one adjacent’, function() {
+  //   expect(swapHelper([2, 1])).toEqual([1, 2]);
+  // });
 });
